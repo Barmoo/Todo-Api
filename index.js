156573f7095c79672d3ab8d14cfda9dff68907f9
Belinda Armoo <armoobelinda92@gmail.com>
2024-10-03 15:23:17 +0000

    changes made

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import todoRouter from './routes/todo.js';
 import userRouter from './routes/user.js';
+import cors from 'cors';
 
 // connect to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -12,6 +13,7 @@ await mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 //use middlewares
+app.use(cors()),
 app.use(express.json());
 //use routes
 app.use(todoRouter);
