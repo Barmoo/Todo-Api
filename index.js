4159afc1a8e6a26920068ceafae2a11c36da46ea
Belinda Armoo <armoobelinda92@gmail.com>
2024-10-14 14:34:35 +0000

    uploads added todo api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import todoRouter from './routes/todo.js';
 import userRouter from './routes/user.js';
-import cors from 'cors';
 
 // connect to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -13,7 +13,7 @@ await mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 //use middlewares
-app.use(cors()),
+app.use(cors());
 app.use(express.json());
 //use routes
 app.use(todoRouter);
