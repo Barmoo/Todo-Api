12f27875733a799c97a3c38e204e441081783f4d
Belinda Armoo <armoobelinda92@gmail.com>
2024-10-16 15:30:48 +0000

    authentication added

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,77 @@
 //register,login,logout
 
-export const registerUser = (req, res, next) => {
-    res.json('user registered');
+import { registerUserValidator, loginUserValidator } from "../validators/user.js";
+import { UserModel } from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async(req, res, next) => {
+    try {
+        //validate user input
+        const{error,value} = registerUserValidator.validate(req.body);
+        if(error){
+            return res.status(422).json(error)
+        }
+        //check if user does not exist
+        const user = await UserModel.findOne({email: value.email});
+        if (user){
+            return res.status(409).json('User already exist!');
+        }
+        //harsh their password
+        const hashedPassword = bcrypt.hashSync(value.password, 10);
+        //save the user into dsatabase
+        await UserModel.create({
+            ...value,
+            password: hashedPassword
+
+        });
+        //send user confirmation email
+        //respond to the request
+    
+        res.json('user registered');
+    
+    } catch (error) {
+        next(error);
+    }    
+    }
+
+export const loginUser = async(req, res, next) => {
+    try {
+        //validate user input
+        const {error,value} = loginUserValidator.validate(req.body);
+        if (error){
+            return res.status(422).json(error);
+        }
+        //find one user with identifier
+        const user = await UserModel.findOne({email: value.email});
+        if (!user) {
+            return res.status(404).json('User does not exist!');
+        }
+        //compare their passwords
+        const correctPassword = bcrypt.compareSync(value.password,user.password);
+        if(!correctPassword){
+            return res.status(401).json('invalid credentials');
+        }
+        //sign a token for user
+        const token = jwt.sign(
+            {id:user.id},
+            process.env.JWT_PRIVATE_KEY,
+            {expiresIn: '24h'}
+        );
+        // respond to request
+        res.json({
+            message: 'User logged in',
+            accessToken: token
+        }),
+        res.json('user logged in successfully');
+    } catch (error) {
+        next(error);
+        
+    }
 }
 
-export const loginUser = (req, res, next) => {
-    res.json('user logged in successfully');
+export const getProfile = (req,res,next)=>{
+    res.json('User profile');
 }
 
 export const logoutUser = (req, res, next) => {
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, updateProfile } from "../controllers/user.js";
+import { loginUser, logoutUser,getProfile, registerUser, updateProfile } from "../controllers/user.js";
 import { userAvatarUpload } from "../middlewares/upload.js";
 
 const userRouter = Router();
 
 userRouter.post('/users/register', registerUser);
+
 userRouter.post('/users/login', loginUser);
+
+userRouter.get('/users/me', getProfile);
+
 userRouter.post('/users/logout', logoutUser);
+
 userRouter.post('/users/me', userAvatarUpload.single('avatar'), updateProfile);
 
 export default userRouter;
